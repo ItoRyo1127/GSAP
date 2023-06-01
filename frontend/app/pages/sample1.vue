1075074d68f31dec6f21d17ab1704177c6d320e2
ItoRyo1127 <1127ajt@gmail.com>
2023-06-01 16:37:37 +0900

    wip

--- frontend/app/pages/sample1.vue
+++ frontend/app/pages/sample1.vue
@@ -22,32 +22,69 @@ onMounted(() => {
     .set("h1", { textContent: "Hide Motion" }, "+=1") // 1秒待機
     .to(
       ".rect1",
-      { y: -32, opacity: 0.5, duration: 0.5, rotate: 360, scale: 100 },
+      { y: -32, opacity: 0, duration: 0.5, rotate: 360, scale: 100 },
       "+=0.5"
     )
     .to(
       ".rect2",
-      { x: 32, y: 32, opacity: 0.5, duration: 0.5, scale: 100 },
+      { x: 32, y: 32, opacity: 0, duration: 0.5, scale: 100 },
       "-=0.4"
     )
     .to(".rect3", { y: -32, opacity: 0.5, duration: 0.5, rotate: 360 }, "-=0.4")
     .to(
       ".rect4",
-      { x: 32, y: 32, opacity: 0.5, duration: 0.5, scale: 100 },
+      { x: 32, y: 32, opacity: 0, duration: 0.5, scale: 100 },
       "-=0.4"
     )
     .to(".rect5", { y: -32, opacity: 0.5, duration: 0.5, rotate: 360 }, "-=0.4")
     .to(
       ".rect6",
-      { x: 32, y: 32, opacity: 0.5, duration: 0.5, scale: 100 },
+      { x: 32, y: 32, opacity: 0, duration: 0.5, scale: 100 },
       "-=0.4"
     )
-    .from(".rect7", { opacity: 0, duration: 1 });
+    .from(".rect7", {
+      opacity: 0,
+      duration: 1,
+      rotate: 360,
+      y: -300,
+      x: -300,
+      skewX: 200,
+      skewY: 200,
+    })
+    .from(".rect8", {
+      opacity: 0,
+      duration: 1,
+      rotate: 360,
+      y: 300,
+      x: 300,
+      skewX: -200,
+      skewY: -200,
+    })
+    .from(".rect9", {
+      opacity: 0,
+      duration: 1,
+      rotate: 360,
+      y: -100,
+      x: -100,
+      skewX: 100,
+      skewY: 100,
+    })
+    .from(".rect10", {
+      opacity: 0,
+      duration: 1,
+      rotate: 360,
+      y: 100,
+      x: 100,
+      skewX: -100,
+      skewY: -100,
+    });
 });
 </script>
 
 <template>
   <div class="container">
+    <div class="rect rect8">🎋</div>
+    <div class="rect rect9">🎋</div>
     <div class="rect rect1">こ</div>
     <div class="rect rect2">う</div>
     <div class="rect rect3">さ</div>
@@ -55,6 +92,7 @@ onMounted(() => {
     <div class="rect rect5">さ</div>
     <div class="rect rect6">ん</div>
     <div class="rect rect7">🎋</div>
+    <div class="rect rect10">🎋</div>
   </div>
 </template>
 
