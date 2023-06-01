587678b098c32ced70c9138dab4c2e608e3e8855
ItoRyo1127 <1127ajt@gmail.com>
2023-06-01 15:06:12 +0900

    wip

--- frontend/app/pages/sample1.vue
+++ frontend/app/pages/sample1.vue
@@ -6,29 +6,39 @@ onMounted(() => {
   gsap
     .timeline({ repeat: -1, repeatDelay: 0.5 })
     .set("h1", { textContent: "Show Motion" })
-    .from(".rect1", { y: -32, opacity: 0, duration: 0.5, rotate: 360 })
+    .from(".rect1", {
+      y: -32,
+      opacity: 0,
+      duration: 0.5,
+      rotate: 360,
+      scale: 1.5,
+    })
     .from(".rect2", { x: 32, y: 32, opacity: 0, duration: 0.5, scale: 1.5 })
     .from(".rect3", { y: -32, opacity: 0, duration: 0.5, rotate: 360 })
     .from(".rect4", { x: 32, y: 32, opacity: 0, duration: 0.5, scale: 1.5 })
     .from(".rect5", { y: -32, opacity: 0, duration: 0.5, rotate: 360 })
     .from(".rect6", { x: 32, y: 32, opacity: 0, duration: 0.5, scale: 1.5 })
     .set("h1", { textContent: "Hide Motion" }, "+=1") // 1秒待機
-    .to(".rect1", { y: -32, opacity: 0.5, duration: 0.5, rotate: 360 }, "+=0.5")
+    .to(
+      ".rect1",
+      { y: -32, opacity: 0.5, duration: 0.5, rotate: 360, scale: 100 },
+      "+=0.5"
+    )
     .to(
       ".rect2",
-      { x: 32, y: 32, opacity: 0.5, duration: 0.5, scale: 1.5 },
+      { x: 32, y: 32, opacity: 0.5, duration: 0.5, scale: 100 },
       "-=0.4"
     )
     .to(".rect3", { y: -32, opacity: 0.5, duration: 0.5, rotate: 360 }, "-=0.4")
     .to(
       ".rect4",
-      { x: 32, y: 32, opacity: 0.5, duration: 0.5, scale: 1.5 },
+      { x: 32, y: 32, opacity: 0.5, duration: 0.5, scale: 100 },
       "-=0.4"
     )
     .to(".rect5", { y: -32, opacity: 0.5, duration: 0.5, rotate: 360 }, "-=0.4")
     .to(
       ".rect6",
-      { x: 32, y: 32, opacity: 0.5, duration: 0.5, scale: 1.5 },
+      { x: 32, y: 32, opacity: 0.5, duration: 0.5, scale: 100 },
       "-=0.4"
     );
 });
